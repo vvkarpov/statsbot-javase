68590c0ba6d9370d87e668429e7556b9ebcaca02
vvkarpov <chuzhoi36@gmail.com>
2021-02-02 15:30:19 +0300

    Added telegram api integration

--- src/ru/vkarpov/bots/Bot.java
+++ src/ru/vkarpov/bots/Bot.java
@@ -1,18 +1,23 @@
 package ru.vkarpov.bots;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-/*
-AdvertStatsBot
-1683780434:AAFYqEOEZNZaze2HhiIboUV36ZOiqgPK5ok
- */
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        if(update.getMessage().getText().equals("/stat")){
+            sendMessage.setText("Statistics for yesterday: 0 visitor's | 0 click's");
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException exp){
+                exp.getStackTrace();
+            }
+        }
     }
 
     @Override
