85307eabf72f9f6cb05f44accba029a14e11592e
vvkarpov <chuzhoi36@gmail.com>
2021-02-03 13:57:22 +0300

    added get request

---
+++ src/ru/vkarpov/bots/controller/GetStats.java
@@ -0,0 +1,37 @@
+package ru.vkarpov.bots.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetStats {
+
+    private static final int CONNECTION_TIMEOUT = 100;
+
+    public static String getStats() throws IOException {
+
+        String link = "";
+        final URL url = new URL(link);
+        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setConnectTimeout(CONNECTION_TIMEOUT);
+        con.setReadTimeout(CONNECTION_TIMEOUT);
+
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            final StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            return content.toString();
+        } catch (final Exception ex) {
+            ex.printStackTrace();
+            return "";
+        }
+    }
+
+}
