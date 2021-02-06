537e375c129dd6ae8eaf0ebab24d568f1430fd58
vvkarpov <chuzhoi36@gmail.com>
2021-02-06 17:51:08 +0300

    added new PostRequest

--- src/ru/vkarpov/bots/Request.java
+++ src/ru/vkarpov/bots/Request.java
@@ -15,60 +15,22 @@ import java.util.Map;
 
 public class Request {
 
-    private static final int CONNECTION_TIMEOUT = 100;
-
     public static String getStats() throws IOException {
 
-        String link = "https://api.direct.yandex.com/json/v5/reports";
-        final URL url = new URL(link);
+        final URL url = new URL("https://api.direct.yandex.com/json/v5/reports");
         final HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
         con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setConnectTimeout(CONNECTION_TIMEOUT);
-        con.setReadTimeout(CONNECTION_TIMEOUT);
-
-        //GET request
-        /*
-        try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-            String inputLine;
-            final StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            return content.toString();
-        } catch (final Exception ex) {
-            ex.printStackTrace();
-            return "";
-        }
-         */
-
-        final Map<String, String> parameters = new HashMap<>();
-        parameters.put("title", "foo");
-        parameters.put("body", "bar");
-        parameters.put("userId", "1");
-
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
         con.setDoOutput(true);
-        final DataOutputStream out = new DataOutputStream(con.getOutputStream());
-        out.writeBytes(getParamsString(parameters));
-        out.flush();
-        out.close();
-    }
 
-    public static String getParamsString(final Map<String, String> params) {
-        final StringBuilder result = new StringBuilder();
+        byte[] out = "{\"username\":\"root\",\"password\":\"password\"}".getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
 
-        params.forEach((name, value) -> {
-            result.append(URLEncoder.encode(name, StandardCharsets.UTF_8));
-            result.append('=');
-            result.append(URLEncoder.encode(value, StandardCharsets.UTF_8));
-            result.append('&');
-        });
-
-        final String resultString = result.toString();
-        return !resultString.isEmpty()
-                ? resultString.substring(0, resultString.length() - 1)
-                : resultString;
-    }
+        con.setFixedLengthStreamingMode(length);
+        con.connect();
+        try(OutputStream os = con.getOutputStream()) {
+            os.write(out);
+        }
+// Do something with http.getInputStream()
 
 }
